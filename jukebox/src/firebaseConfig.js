8ac57a9e75d6e27e2230c04e9ad549283c0092d5
Lior Levy <lior.levy7@t-mobile.com>
2019-05-13 19:42:55 -0700

    got sign in to work

---
+++ jukebox/src/firebaseConfig.js
@@ -0,0 +1,11 @@
+export default {
+    apiKey: "AIzaSyAMh_nALUGXpqRI2Ce0LJD-bQnU_UT_Uoc",
+    authDomain: "jukebox-d030f.firebaseapp.com",
+    databaseURL: "https://jukebox-d030f.firebaseio.com",
+    projectId: "jukebox-d030f",
+    storageBucket: "jukebox-d030f.appspot.com",
+    messagingSenderId: "686175886055",
+    // appId: "1:686175886055:web:864d7ae6920af354"
+};
+
+// export default config;
