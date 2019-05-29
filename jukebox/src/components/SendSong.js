84c733e1c061ae618228e6aa3b90da44fb1071b4
Lior Levy <lior7212@gmail.com>
2019-05-29 16:00:44 -0700

    up

--- jukebox/src/components/SendSong.js
+++ jukebox/src/components/SendSong.js
@@ -16,22 +16,24 @@ class SendSong extends Component {
         data.on("value", function (snapshot) {
             let res = snapshot.val();
 
-            var arr = [];
-            Object.keys(res).forEach(function (key) {
-                arr.push(res[key]);
-            });
+            // var arr = [];
+            // Object.keys(res).forEach(function (key) {
+            //     arr.push(res[key]);
+            // });
+            if (res['userAccepted'] != "") {
 
-            if (arr[0] == "true" && props.history.location.state) {
-                // console.log("accepted");
-                // props.history.push({pathname: '/', state: {id: props.history.location.state['id'], title: props.history.location.state['title'], artist: props.history.location.state['artist']}});
-                // let title = props.history.location.state['title'].split(' ').join('-');
-                // let artist = props.history.location.state['artist'].split(' ').join('-');
-                props.history.push(`/`)
-            } else if (arr[0] == "false") {
-                // console.log("rejected");
-                // let title = props.history.location.state['title'].split(' ').join('-');
-                // let artist = props.history.location.state['artist'].split(' ').join('-');
-                props.history.push(`/`)
+                if (res['userAccepted'] == "true" && props.history.location.state) {
+                    // console.log("accepted");
+                    // props.history.push({pathname: '/', state: {id: props.history.location.state['id'], title: props.history.location.state['title'], artist: props.history.location.state['artist']}});
+                    // let title = props.history.location.state['title'].split(' ').join('-');
+                    // let artist = props.history.location.state['artist'].split(' ').join('-');
+                    props.history.push(`/`)
+                } else if (res['userAccepted'] == "false") {
+                    // console.log("rejected");
+                    // let title = props.history.location.state['title'].split(' ').join('-');
+                    // let artist = props.history.location.state['artist'].split(' ').join('-');
+                    props.history.push(`/`)
+                }
             }
 
         }, function (errorObject) {
@@ -46,13 +48,13 @@ class SendSong extends Component {
         });
 
         return (
-            <div style = {{textAlign: 'center', marginRight: '160px'}}>
+            <div style={{ textAlign: 'center', marginRight: '160px' }}>
                 {/* <h1 style={{ color: 'white', fontSize: '50px' }}>Selected song was sent to your peers </h1> */}
-                {currUser == "Allen Building" ? <div><h1 style={{ color: 'white', fontSize: '50px'}}>Selected song was sent to your peers in the <span style={{ color: '#46C4D3' }}>Research Commons</span></h1> </div> : 
-                                                <div><h1 style={{ color: 'white', fontSize: '50px' }}>Selected song was sent to your peers in the <span style={{ color: '#FFF170' }}>Jaech</span> </h1></div>}
-                <h1 style = {{marginTop: '45px', color: 'white'}}>They only know the genre and artist of the song</h1>
-                {currUser == "Allen Building" ? <img style = {{marginTop: '10px'}} src = "https://i.imgur.com/3xhwCSf.png"></img> : 
-                                                <img style = {{marginTop: '10px'}} src = "https://i.imgur.com/DlebHZY.png"></img>}
+                {currUser == "Allen Building" ? <div><h1 style={{ color: 'white', fontSize: '50px' }}>Selected song was sent to your peers in the <span style={{ color: '#46C4D3' }}>Research Commons</span></h1> </div> :
+                    <div><h1 style={{ color: 'white', fontSize: '50px' }}>Selected song was sent to your peers in the <span style={{ color: '#FFF170' }}>Jaech</span> </h1></div>}
+                <h1 style={{ marginTop: '45px', color: 'white' }}>They only know the genre and artist of the song</h1>
+                {currUser == "Allen Building" ? <img style={{ marginTop: '10px' }} src="https://i.imgur.com/3xhwCSf.png"></img> :
+                    <img style={{ marginTop: '10px' }} src="https://i.imgur.com/DlebHZY.png"></img>}
                 {/* <img src = "https://i.imgur.com/3xhwCSf.png"></img> */}
             </div>
         );
